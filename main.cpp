50a92130ae3d7ead4965c079b924ac1092ba91cc
Joel Guerra <joelaguerra1@gmail.com>
2017-10-09 10:56:32 -0700

    inital commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+void Setup()
+{
+    gameOver = false
+}
+void Draw()
+{
+
+}
+void Input()
+{
+
+}
+void Logic()
+{
+
+}
+int main()
+{
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
+}
