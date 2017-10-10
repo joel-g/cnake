e5f2bade68d1afe733ad35e5e5e97d7c3f77b886
Joel Guerra <joelaguerra1@gmail.com>
2017-10-10 11:06:47 -0700

    defines Setup()

--- main.cpp
+++ main.cpp
@@ -4,10 +4,18 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
 
 void Setup()
 {
     gameOver = false
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % width;
+    score = 0;
 }
 void Draw()
 {
